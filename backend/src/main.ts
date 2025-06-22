4153ce72ed6b497ce415b0bbc23af638d3d3e477
vadikghetta <helloiamvadik@mail.ru>
2025-06-22 21:00:15 +0300

    add express

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+import express from "express";
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+	res.send("pong");
+});
+
+app.listen(8080, () => {
+	console.info("Listening in http://localhost:8080");
+});
