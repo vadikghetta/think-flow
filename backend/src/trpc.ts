88713276795e9308e80540cfaec7ed679ea0d693
vadikghetta <helloiamvadik@mail.ru>
2025-06-29 22:05:42 +0300

    add by slug

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,6 @@
 import { initTRPC } from "@trpc/server";
 import _ from "lodash";
+import { z } from "zod";
 
 const thoughts = _.times(100, (i) => ({
 	nick: `cool-article-nick-${i}`,
@@ -13,7 +14,17 @@ const trpc = initTRPC.create();
 export const trpcRouter = trpc.router({
 	getThoughts: trpc.procedure.query(() => {
 		return { thoughts: thoughts.map((item) => _.pick(item, ["nick", "name", "description"])) };
-	})
+	}),
+	getArticle: trpc.procedure
+		.input(
+			z.object({
+				slug: z.string()
+			})
+		)
+		.query(({ input }) => {
+			const element = thoughts.find((el) => el.nick === input.slug);
+			return { article: element ?? null };
+		})
 });
 
 export type TrpcRouter = typeof trpcRouter;
